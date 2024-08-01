0352979522e67e486d26f55a0a14cf6a9a539dc7
Eugene <eugene.yuriev@px.com>
2024-08-02 01:47:03 +0300

    fix core shit, fix primeNg, add Dock menu

--- always_second_v2/src/app/app.component.ts
+++ always_second_v2/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ always_second_v2/src/app/app.module.ts
@@ -0,0 +1,21 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {GalleriaModule} from "primeng/galleria";
+import {DockModule} from "primeng/dock";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    GalleriaModule,
+    DockModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
