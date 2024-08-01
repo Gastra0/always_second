424222c5b23536985d4fcbe532e811227c85b0cb
Eugene <eugene.yuriev@px.com>
2024-08-02 02:42:35 +0300

    add fonts

--- always_second_v2/src/app/app.component.ts
+++ always_second_v2/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'always_second_v2';
   dockItems = [{label: "Intro"}, {label: "Given"}, {label: "Donaters"}, {label: "Report"}, {label: "Receivers"}];
+  csvUrlReceivers = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTdJn_gZe3zyt7S6LKUYQj6lOgZ6RLPTNB7gLs_k-j_GPN9o3SWvDrqTN4lA-AFsvdNkzq5Rhze1Px9/pub?output=csv&range=H3";
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+    let response = this.httpClient.get(this.csvUrlReceivers, {responseType: 'text'});
+    response.subscribe(data => {
+      console.log(data);
+
+    })
+
+  }
+
 }
+
+
+
--- always_second_v2/src/app/app.module.ts
+++ always_second_v2/src/app/app.module.ts
@@ -1,9 +1,10 @@
-﻿import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+﻿import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {GalleriaModule} from "primeng/galleria";
 import {DockModule} from "primeng/dock";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -12,10 +13,12 @@ import {DockModule} from "primeng/dock";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     GalleriaModule,
     DockModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
