9ec58a9cd4822743fe7356f79d450d67a856041e
Eugene <eugene.yuriev@px.com>
2024-11-13 19:30:55 +0200

    replace main

--- always_second_v2/src/app/app.component.ts
+++ always_second_v2/src/app/app.component.ts
@@ -1,4 +1,14 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit, QueryList,
+  ViewChild,
+  ViewChildren,
+  ViewEncapsulation
+} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {TranslocoService} from "@jsverse/transloco";
 import {MenuItem} from "primeng/api";
@@ -10,8 +20,17 @@ import {MenuItem} from "primeng/api";
   encapsulation: ViewEncapsulation.None
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewChecked {
   title = 'always_second_v2';
+  @ViewChildren('page') pages!: QueryList<ElementRef>;
+  @ViewChild('prev') prev!: ElementRef;
+  @ViewChild('next') next!: ElementRef;
+
+  idlePeriod = 100;
+  animationDuration = 1000;
+  lastAnimation = 0;
+  index = 0;
+
   dockItems: MenuItem[] = [{label: "Intro"}, {label: "Given"}, {label: "Donaters"}, {label: "Report"}, {label: "Receivers"}];
   csvUrlReceivers = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTdJn_gZe3zyt7S6LKUYQj6lOgZ6RLPTNB7gLs_k-j_GPN9o3SWvDrqTN4lA-AFsvdNkzq5Rhze1Px9/pub?output=csv&range=H3";
   localeItems: MenuItem[] = [{
@@ -37,9 +56,77 @@ export class AppComponent implements OnInit {
     })
   }
 
+  togglePageContent(index: number, state: string) {
+    console.log("attempt to toggle")
+    if (state === 'show') {
+      this.pages
+        .toArray()
+        [index].nativeElement.querySelector('.page-content')
+        .classList.add('show');
+    } else {
+      this.pages
+        .toArray()
+        [index].nativeElement.querySelector('.page-content')
+        .classList.remove('show');
+    }
+  }
+
+  ngAfterViewChecked() {
+    this.togglePageContent(0, 'show');
+  }
+
   changeLocale(locale: string) {
     this.activeLang = locale;
     this.translocoService.setActiveLang(locale);
+  }
+
+  clickPrev() {
+    if (this.index < 1) return;
+    this.togglePageContent(this.index, 'hide');
+    this.index--;
+    this.pages.forEach((page, i) => {
+      if (i === this.index) {
+        this.togglePageContent(i, 'show');
+        page.nativeElement.scrollIntoView({behavior: 'smooth'});
+      }
+    });
+  }
+
+  clickNext() {
+    if (this.index > 3) return;
+    this.togglePageContent(this.index, 'hide');
+    this.index++;
+    this.pages.forEach((page, i) => {
+      if (i === this.index) {
+        this.togglePageContent(i, 'show');
+        page.nativeElement.scrollIntoView({behavior: 'smooth'});
+      }
+    });
+  }
+
+  @HostListener('wheel', ['$event'])
+  onMouseWheel(event: WheelEvent) {
+    let delta = 0;
+    if (event['deltaMode']) {
+      delta = event['deltaMode'];
+    }
+    const timeNow = new Date().getTime();
+
+    if (
+      timeNow - this.lastAnimation <
+      this.idlePeriod + this.animationDuration
+    ) {
+      event.preventDefault();
+      return;
+    }
+
+    if (delta < 0) {
+      this.next.nativeElement.click();
+    } else {
+      this.prev.nativeElement.click();
+    }
+
+    this.lastAnimation = timeNow;
   }
 
 }
